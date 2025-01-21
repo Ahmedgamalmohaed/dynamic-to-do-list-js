83d999337b8adaeb2023a1babc99e30dbfcd513d
Ahmed gamal <144253822+Ahmedgamalmohaed@users.noreply.github.com>
2025-01-21 17:07:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 ["document.addEventListener", "DOMContentLoaded", "function"]
 ["addButton", "add-task-btn", "taskInput", "task-input", "taskList", "task-list"]
 ["addTask", "taskInput.value.trim()", "taskText", "alert"]
+["document.createElement", "button", "Remove", "remove-btn", "classList.add", "appendChild"]
