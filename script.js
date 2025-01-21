61d2b19cb59ea62c38699a89ccc7c2acae47f617
Ahmed gamal <144253822+Ahmedgamalmohaed@users.noreply.github.com>
2025-01-21 17:06:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 ["document.addEventListener", "DOMContentLoaded", "function"]
 ["addButton", "add-task-btn", "taskInput", "task-input", "taskList", "task-list"]
+["addTask", "taskInput.value.trim()", "taskText", "alert"]
