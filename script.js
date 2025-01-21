fc510974c5eafa00827a6036fe60b7408d67e169
Ahmed gamal <144253822+Ahmedgamalmohaed@users.noreply.github.com>
2025-01-21 17:08:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,4 @@
 ["document.createElement", "button", "Remove", "remove-btn", "classList.add", "appendChild"]
 ["addButton.addEventListener", "click"] / script.js doesn't contain: ["taskInput.addEventListener", "keypress", "Enter", "event.key"]
 ["loadTasks"]
+["localStorage.getItem", "tasks"]
